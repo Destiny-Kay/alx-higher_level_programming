9faeb0093ef6401cb579abd7d61d20627e3026ba
Destiny-kaye <destinykevogo@gmail.com>
2023-09-11 06:28:24 +0300

    fix, matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers"""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != len(matrix[i]) - 1:
-                print("", end=" ")
+    for r in matrix:
+        for c in r:
+            print("{:d}".format(c), end=" " if c != r[-1] else "")
         print("$")
