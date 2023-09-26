4f71ee268815bbaa0803a5ff3aff0be48b6dede5
Destiny-kaye <destinykevogo@gmail.com>
2023-09-26 05:18:04 +0300

    Fix: 3-safe_print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         res = None
     finally:
-        print("Inside result:{}".format(res))
+        print("Inside result: {}".format(res))
