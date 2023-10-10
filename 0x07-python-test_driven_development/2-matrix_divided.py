7a8b9948ab7f80a9a326750cb8ce4a17adce7c72
Destiny-kaye <destinykevogo@gmail.com>
2023-10-10 13:42:49 +0300

    Add: new files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3\
+'''
+Divides all elements of a matrix
+'''
+
+def matrix_divided(matrix, div):
+    '''
+    Divides a matrix by div
+
+    Args:
+        matrix: the matrix to be divided
+        div: the divisor to t hea matrix
+    Returns:
+        A new matrix
+    '''
+    
