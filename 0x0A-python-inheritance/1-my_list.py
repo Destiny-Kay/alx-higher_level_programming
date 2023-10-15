37b6de4cdfc32d0fff27e91b57f1e98ebed270e9
Destiny-kaye <destinykevogo@gmail.com>
2023-10-15 17:28:36 +0300

    Fix: pycodestyle validation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""Mylist class definition"""
 
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+class MyList(list):
+    '''Mylist class definition'''
+    def print_sorted(self):
+        '''prints Mylist in a sorted(ascending) order'''
+        print(sorted(self))
