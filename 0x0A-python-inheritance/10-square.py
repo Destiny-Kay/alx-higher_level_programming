f2cc29842b45824e2b93eb149e62713d6e9d873c
Destiny-kaye <destinykevogo@gmail.com>
2023-10-15 21:58:59 +0300

    Add: more files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''A square class definition'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''The rectangle class definition'''
+    def __init__(self, size):
+        '''Square class instantiation'''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size =  size
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Rectangle class definition'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class definition'''
+    def __init__(self, width, height):
+        '''Instantiates the rectangle class'''
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
