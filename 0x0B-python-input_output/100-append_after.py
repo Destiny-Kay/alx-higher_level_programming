afa0f4364573709ef257802a76108957aab7e1f6
Destiny-Kay <destinykevogo@gmail.com>
2023-12-06 16:49:39 +0300

    Add: appending files

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Inserts a line of text to a file that meets a certain criterion'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''
+    Appends a string to a file after a search pattern in met
+    Args:
+        filename: name of the file
+        search-string: search pattern to be looked for
+        new_string: the nw string to be appended
+    '''
+    text=''
+    with open(filename) as f:
+        for line in f:
+            text+=line
+            if search_string in line:
+                text += new_string
+    with open(filename, 'w') as w:
+        w.write(text)
