10edfaf12690a072a32019382f2707dfb23d64d0
Destiny-Kay <destinykevogo@gmail.com>
2024-03-05 10:57:24 +0300

    Compress files to be 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 #bash script that returns the size of body in bytes
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-echo "$response"
+response=$(curl -s -w "%{size_download}" -o /dev/null "$1") && echo "$response"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 #Sends a GET request to the url and sets a header to it
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-response=$(curl -s -X GET -H "X-School-User-Id: 98" "$url")
-echo "$response"
+response=$(curl -s -X GET -H "X-School-User-Id: 98" "$1") && echo "$response"
